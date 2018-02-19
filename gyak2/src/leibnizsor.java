111e5effa3057e1149f4db2c4171a3b31e3d5b95
karancsi <karancsi@lab104-14.iit.uni-miskolc.hu>
2018-02-19 16:29:07 +0100

    :)

--- gyak2/src/leibnizsor.java
+++ gyak2/src/leibnizsor.java
@@ -10,9 +10,25 @@ public class leibnizsor {
 
 	public static void main(String[] args) {
 		//Leibniz-féle sor
-		double pi=1;
+		double pi=0;
+		int darab = 4000;
+		int elojel =1;
+		for (int i = 1; i <= darab; i+=2) {
+			pi +=elojel*1.0/i;
+			elojel*=-1;
+			
+		}
 		
-
+		System.err.println("Leibniz-féle sor: ");
+		System.out.println("pi/4 erteke:" + pi);
+		System.out.println("pi erteke:" + pi*4);
+	}
+	//Faktoriális
+	
+	
+	
+	
 	}
 
-}
+
+
