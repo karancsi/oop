7de5432805d171f664cd19b4b422fc1b69d11c56
karancsi <karancsi@lab104-20.iit.uni-miskolc.hu>
2018-04-10 16:41:21 +0200

    :D:D

--- gyak tompaval_2/src/forgástest/forgástestfut.java
+++ gyak tompaval_2/src/forgástest/forgástestfut.java
@@ -1,11 +1,20 @@
 package forgástest;
 import forgástest.Henger;
+import forgástest.Téglatest;;
 public class forgástestfut {
 
 	
 	public static void main(String[] args) {
-		Henger h1 = new Henger();
-
+		Henger h1 = new Henger(5,10);
+		System.out.println(h1.toString());
+		System.out.println("A h1 térfogata:"+ h1.getVolume());
+		System.out.println("");
+		
+		
+		Téglatest t1 = new Téglatest(2, 5, 3);
+		System.out.println(h1.toString());
+		System.out.println("A t1 térfogata:"+ t1.getVolume());
+		System.out.println("");
 	}
 
 }
